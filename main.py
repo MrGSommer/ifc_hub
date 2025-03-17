b24ebc7fddb031a26e9bc2d00a21803ca20e6e34
MrGSommer <gsommer@companytransfer.ch>
2025-03-17 10:49:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from ifc_requirements import app as ifc_requirements_app
 from ifc_remover import app as ifc_remover_app
+from ifc_viewer import app as ifc_viewer_app
 
 st.set_page_config(page_title="IFC Operation Hub", layout="wide")
 st.title("IFC Operation Hub")
 st.markdown("Wählen Sie einen Tab für verschiedene IFC-Operationen.")
 
-tabs = st.tabs(["Übersicht & Tool-Erklärungen", "IFC Element Remover"])
+tabs = st.tabs(["Übersicht & Tool-Erklärungen", "IFC Element Remover", "3D Viewer"])
 
 with tabs[0]:
     ifc_requirements_app()
 
 with tabs[1]:
     ifc_remover_app()
+
+with tabs[2]:
+    ifc_viewer_app()
